1c7db586f0beee0dd9ff23def8687b49f7f72a5f
akioweh <0@akioweh.com>
2025-08-24 18:17:47 +0400

    fix and enhance kmp
    fix dfa builder + add dfa matcher

--- templates/string.py
+++ templates/string.py
@@ -2,12 +2,14 @@
 balls.
 """
 
-from collections import defaultdict
 from typing import Iterator, Sequence
 
 
-def lsp(s: Sequence) -> list[int]:
-    """``lsp(s)[i]`` is the length of
+def lsp[T](s: Sequence[T]) -> list[int]:
+    """Computes the "suffix-prefix array" of s
+    in O(|s|).
+
+    ``lsp(s)[i]`` is the length of
     the longest proper suffix of ``s[:i]``
     which is also a prefix of ``s[:i]``.
     """
@@ -23,37 +25,33 @@ def lsp(s: Sequence) -> list[int]:
     return res
 
 
-def dfa(s: Sequence) -> list[dict[..., int]]:
-    """Constructs a DFA that accepts ``L = {w | w.endswith(s)}``.
+def dfa[T](s: Sequence[T]) -> list[dict[T, int]]:
+    """Constructs a DFA that accepts ``L = {w | w.endswith(s)}``
+    in O(|s|*|S|) where S is the alphabet.
+
     ``res[i][c] -> j`` is a transition from state ``i`` to state ``j`` consuming ``c``.
     State ``0`` is the start state; ``len(s)`` is the accepting state.
     In addition, the DFA is self-looping; it can be used to
-    find all occurrences of ``s`` in a string.
+    find multiple occurrences of ``s`` in a string.
     """
-    N = len(s)
-    res = [
-        defaultdict(int)
-        for _ in range(N + 1)
-    ]
     alphabet = set(s)
-    for i in range(N + 1):
-        for c in alphabet:
-            j = i
-            while j and s[j] != c:
-                j = res[j][c]
-            if s[j] == c:
-                j += 1
-            res[i][c] = j
-    for i in range(N + 1):
-        for c in alphabet:
-            if c not in res[i]:
-                res[i][c] = res[res[i][c]][c]
+    res: list[dict[T, int]] = []
+    cur = {c: 0 for c in alphabet}
+    for i, t in enumerate(s):
+        res.append(cur.copy())
+        res[i][t] = i + 1
+        cur = res[cur[t]]
+    # looping transitions
+    res.append(cur.copy())
+
     return res
 
 
 def matches_lsp(s: str, p: str, _lsp: list[int]) -> Iterator[int]:
-    """Yields all indices of occurrences of ``p`` in ``s``.
-    KMP using suffix-prefix array.
+    """Yields all occurrences of ``p`` in ``s``
+    by their starting indices in ``s``.
+
+    KMP algorithm using a suffix-prefix array.
     """
     N = len(s)
     M = len(p)
@@ -72,17 +70,45 @@ def matches_lsp(s: str, p: str, _lsp: list[int]) -> Iterator[int]:
                 i += 1
 
 
+def matches_dfa(s: str, p: str, _dfa: list) -> Iterator[int]:
+    """Yields all occurrences of ``p`` in ``s``
+    by their starting indices in ``s``.
+
+    KMP algorithm using a DFA.
+    """
+    M = len(p)
+    j = 0
+    for i, c in enumerate(s):
+        j = _dfa[j].get(c, 0)
+        if j == M:
+            yield i - M + 1
+
+
 if __name__ == '__main__':
-    S = 'e' * 1
-    P = 'eeeeeee'
+    S = r'abcoingoa bcboiiinabccg a faabce fz( fa_ fj;abckla;owi-jadsfj \ak_fj e e aabczd e a_f in the rdb ininininit abcdba abcabcabcabc'
+    P = 'abc'
     _lsp = lsp(P)
     print(_lsp)
-    _res = list(matches_lsp(S, P, _lsp))
-    print(_res)
-    # print(S)
-    # for _i in range(len(S)):
-    #     if _i in _res:
-    #         print('^', end='')
-    #     else:
-    #         print(' ', end='')
-    # print()
+    _res1 = list(matches_lsp(S, P, _lsp))
+    print(_res1)
+    print(S)
+    for _i in range(len(S)):
+        if _i in _res1:
+            print('^', end='')
+        else:
+            print(' ', end='')
+    print('\n\n')
+
+    _dfa = dfa(P)
+    for i, r in enumerate(_dfa):
+        print(f'{i}  {r}')
+    _res2 = list(matches_dfa(S, P, _dfa))
+    print(_res2)
+    print(S)
+    for _i in range(len(S)):
+        if _i in _res2:
+            print('^', end='')
+        else:
+            print(' ', end='')
+    print('\n')
+    print(_res1 == _res2)
